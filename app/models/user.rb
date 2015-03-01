d6f257ebec6f0cd2844901c2c374c2999660053c
Keith <n3v3rsumm3r@gmail.com>
2015-03-01 02:22:16 +0000

    user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  
+  belongs_to :location
+  belongs_to :department
+  belongs_to :group
+  belongs_to :position
+  
+  validates :first_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
