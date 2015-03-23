068b922ce38b89b3d86014d4af228fdf44dd2494
Keith <n3v3rsumm3r@gmail.com>
2015-03-23 01:34:11 +0000

    First attempt at seed, department, location, position, group, and Spencer Wilow User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Department.create(name: 'IT')
+
+Location.create(name: 'Chicago')
+
+Position.create(name: 'Engineer')
+
+Group.create(name: 'Development')
+
+User.create(first_name: 'Spencer', last_name: 'Wilow', email: 'swilow@example.com', department_id: '1', location_id: '1', position_id: '1', group_id: '1', manager: 'Anne Chen')
