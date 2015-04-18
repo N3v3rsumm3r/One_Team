4ebdb7fde388b81bd877fa2a7d4185bf9bb1329f
Keith <n3v3rsumm3r@gmail.com>
2015-04-18 21:39:47 +0000

    additional change to migratio for POSTGRE

--- db/migrate/20150412194157_change_manager_from_string_to_integer_in_users.rb
+++ db/migrate/20150412194157_change_manager_from_string_to_integer_in_users.rb
@@ -1,6 +1,6 @@
 class ChangeManagerFromStringToIntegerInUsers < ActiveRecord::Migration
   def change
     rename_column :users , :manager, :manager_id
-    change_column :users, :manager_id, 'integer USING CAST(column_name AS integer)'
+    change_column :users, :manager_id, 'integer USING CAST(manager_id AS integer)'
   end
 end
