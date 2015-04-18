949aca7a1e8af8ad253ab95fafab29f572d3a59b
Keith <n3v3rsumm3r@gmail.com>
2015-04-18 21:25:46 +0000

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,56 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Department.create(name: 'IT')
+Department.create!([
+  {name: "IT"}
+])
 
-Location.create(name: 'Chicago')
+Location.create!([
+  {name: "Chicago"},
+  {name: "Boston"},
+  {name: "Houston"},
+  {name: "San Francisco"},
+  {name: "London"},
+  {name: "Mumbai"}
+  ])
 
-Position.create(name: 'Engineer')
+Position.create!([
+  {name: "Engineer"},
+  {name: "Analyst"},
+  {name: "Project Lead"},
+  {name: "UI Specialist"}
+  ])
 
-Group.create(name: 'Development')
+Group.create!([
+  {name: "Development"},
+  {name: "Interface Design"},
+  {name: "QA"},
+  {name: "Infrastructure"}
+  ])
 
-Skill.create(name: 'Java')
+Skill.create!([
+  {name: "Java"},
+  {name: "C++"},
+  {name: "C#"},
+  {name: "MS SQL Server"},
+  {name: "MySQL"},
+  {name: "PHP"},
+  {name: "Ruby"},
+  {name: "HTML"},
+  {name: "CSS"},
+  {name: "Flash"},
+  {name: "ASP.Net"},
+  {name: "Test Script Writing"},
+  {name: "Functional Testing"},
+  {name: "Performance Testing"},
+  {name: "Windows"}
+  ])
 
-User.create(first_name: 'Spencer', last_name: 'Wilow', email: 'swilow@example.com', department_id: '1', location_id: '1', position_id: '1', group_id: '1', manager: 'Anne Chen', years_with_company: '3')
+User.create!([
+  {first_name: "Anne", last_name: "Chen", email: "achen@example.com", department_id: "1", location_id: "1", position_id: "3", group_id: "1", manager_id: "1", years_with_company: "8", password_digest: "$2a$10$1HXVcw71j3WKbkIAGOhBd.rwHBEtiFo2/46aERA0cKU8nd6AZ39Zu"}
+  {first_name: "Spencer", last_name: "Wilow", email: "swilow@example.com", department_id: "1", location_id: "1", position_id: "1", group_id: "1", manager_id: "1", years_with_company: "3", password_digest: "$2a$10$1HXVcw71j3WKbkIAGOhBd.rwHBEtiFo2/46aERA0cKU8nd6AZ39Zu"}
+  ])
 
-Project.create(name: 'Example Project 1', description: 'An example of how a real project may look', start_date: '2015-03-28', end_date: '2015-03-29', user_id: '1')
+Project.create!([
+  {name: 'Example Project 1', description: 'An example of how a real project may look', start_date: '2015-03-28', end_date: '2015-03-29', user_id: '1'}
+  ])
