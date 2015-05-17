fe45322f03ca5a7571000d7450b0e270aa4dd01d
Keith <n3v3rsumm3r@gmail.com>
2015-05-17 20:57:53 +0000

    add remember_digest to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406035515) do
+ActiveRecord::Schema.define(version: 20150517205700) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "request_id"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20150406035515) do
     t.string   "password_digest"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "remember_digest"
   end
 
 end
