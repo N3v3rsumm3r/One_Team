4108775f8921eef91464655c425511513b4a67d6
Keith <n3v3rsumm3r@gmail.com>
2015-05-26 00:52:34 +0000

    fixed admin user definition, fix to enable heroku proper functioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,13 @@ class ApplicationController < ActionController::Base
   private
   
   def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
+  
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
