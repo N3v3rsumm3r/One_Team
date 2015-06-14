b4c7697d9aca7beb80f14ec4c62bd91b87d3d15c
Keith <n3v3rsumm3r@gmail.com>
2015-06-14 02:08:59 +0000

    Modifed text message for nil value check in helper

--- app/helpers/nil_value_helper.rb
+++ app/helpers/nil_value_helper.rb
@@ -2,7 +2,7 @@ module NilValueHelper
   
   #check nil value for display in views.
   def request_if_missing(user, association, attribute)
-    user.try(association).try(attribute) || "Please update your profile. HELPER"
+    user.try(association).try(attribute) || "Please update your profile."
   end
   
 end
