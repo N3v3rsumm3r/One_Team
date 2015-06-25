b419ea3564adde1cbb040308efae42d9caf45942
n3v3rsumm3r <n3v3rsumm3r@gmail.com>
2015-06-25 23:01:45 +0000

    removed unneeded method in request helper

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,9 +1,5 @@
 module RequestsHelper
     
-    def response_exists(this_request, this_user)
-        @responses.where(:request == this_request && :user == this_user).any?
-    end
-    
     def current_response(this_request, this_user)
         Response.find_by(:request => this_request, :user => this_user)
     end
