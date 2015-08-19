cc441b03dd03f382d0ba86d8d9db68b75b2d4128
n3v3rsumm3r <n3v3rsumm3r@gmail.com>
2015-08-19 02:20:38 +0000

    enabled admin tab to act as active tab

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -7,4 +7,10 @@ module RequestsHelper
     def current_assignment(this_request, this_user)
         Assignment.find_by(:request => this_request, :user => this_user)
     end
+    
+    def admin_active
+        params[:controller] == 'departments' || params[:controller] == 'locations' || params[:controller] == 'groups'|| 
+        params[:controller] == 'locations' || params[:controller] == 'positions' || params[:controller] == 'skills' ||
+        (params[:controller] == 'users' && !current_page?(current_user))
+    end
 end
